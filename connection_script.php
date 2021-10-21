d9dc1aa36b3f5a6d160572f1d4f4220af24da5ea
name <rezorro228@gmail.com>
2021-10-21 09:48:34 +0300

    10%

--- connection_script.php
+++ connection_script.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "127.0.0.1";
 $username = "root";
-$password = "root";
+$password = "";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, 'clock');
