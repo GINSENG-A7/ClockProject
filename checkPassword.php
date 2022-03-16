4de332568849be36940fde7551db6dec3a3b9052
GINSENG-A7 <zhenya_bykov_1998@mail.ru>
2022-03-16 21:20:08 +0300

    authorisationProviding_Pt1

--- checkPassword.php
+++ checkPassword.php
@@ -3,18 +3,18 @@
         $login = $_POST['Login'];
         $password = $_POST['Password'];
 
-        $link = mysqli_connect('localhost','root','','users');
+        $link = mysqli_connect("127.0.0.1", "root", "root", "clockusers");
 
         if ($link == false) {
             echo "Ошибка подключения!";
         }
-        $query = mysqli_query($link, "SELECT * FROM user WHERE loginUser = '$login'");
+        $query = mysqli_query($link, "SELECT * FROM users WHERE `login` = '$login'");
 
         if (mysqli_num_rows($query)) {
 
             $detail = mysqli_fetch_assoc($query);
 
-            if (hash_equals(md5($password), $detail['passwordUser'])) {
+            if (hash_equals(md5($password), $detail['password'])) {
                 setcookie('User', $login, time() + 3600); 
                 header("location: ./admin.php");
             } 
