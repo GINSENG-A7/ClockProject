f75d23c500025a2c6781b65e6cedb281d70cfb44
GINSENG-A7 <zhenya_bykov_1998@mail.ru>
2022-05-17 02:31:40 +0300

    after_PHPMailer

--- create_ticket_scrpt.php
+++ create_ticket_scrpt.php
@@ -1,4 +1,10 @@
-<?require_once("./connection_script.php")?>
+<?
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require_once("./connection_script.php");?>
+<?require "./phpmailer/src/Exception.php";?>
+<?require "./phpmailer/src/PHPMailer.php";?>
 <?session_start()?>
 <?
 	if (isset($_SESSION["login"])) {
@@ -8,7 +14,36 @@
 	else {
 		$login = NULL;
 	}
-	if(isset($_POST['userId']) && isset($_POST['theme'])) {
-		
+	if(isset($_POST['userId']) && isset($_POST['theme']) && isset($_POST['message'])) {
+		$user = SelectUserByLogin($clockUsersConn, $login);
+		SendMail($user['email']);
+		print_r($user['email']);
 	}
+
+function SendMail($user_email) {
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language');
+	$mail->IsHTML(true);
+
+	$mail->setFrom('genshen21@gmail.com', 'Интернет магазин ювелирных украшений "K.Max Jeveler"');
+	$mail->addAddress($user_email);
+	$mail->Subject = 'Администрация сайта получила ваше сообщение.';
+
+	$body = "<h2>Мы получили ваше письмо и уже работаем над вашим вопросом.</h2>";
+	$body .= "<br>";
+	$body .= "<h3>Вскоре с вами свяжется наш менеджер для дальнейшего разрешения вашего вопроса.</h3>";
+	$body .= "<br>";
+	$body .= "<h4>Данное письмо сгенерированно автоматически. Пожалуйста, не отвечайте на него.</h4>";
+
+	$mail->Body = $body;
+
+	if (!$mail->send()) {
+		$message = 'Ошибка отправки письма';
+	}
+	else {
+		$message = 'Данные отправлены!';
+	}
+	print_r($message);
+}
 ?>
